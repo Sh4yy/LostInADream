45e406f8c390104a7ea913d795a60c51c8226812
shayan <shayan@umd.edu>
2019-12-07 23:03:57 -0500

    added player movement

---
+++ Assets/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offset;
+    public float pitch = 2;
+    private float currentZoom = 10;
+    public float zoomSpeed = 4f;
+    public float minZoom = 5;
+    public float maxZoom = 15;
+
+    public float yawSpeed = 100f;
+    public float pitchSpeed = 10f;
+    private float currentYaw = 0f;
+    private float currentPitch = 0f;
+
+    private void Update()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+        transform.RotateAround(target.position, Vector3.up, currentYaw);
+        // transform.RotateAround(target.position, Vector3.left, currentPitch);
+        transform.position += transform.up * currentPitch * 0.2f;
+    }
+}
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -9,10 +9,21 @@ public class PlayerScript : MonoBehaviour
     public Camera cam;
     public NavMeshAgent agent;
 
+    private void Awake()
+    {
+       agent.updateRotation = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        // moveWithMouse();       
+        moveWithKeyboard();
+    }
+
+
+    void moveWithMouse()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -24,6 +35,20 @@ public class PlayerScript : MonoBehaviour
             }
 
         }
+    }
 
+    void moveWithKeyboard()
+    {
+        float horInput = Input.GetAxis("Horizontal");
+        float verInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horInput, 0f, verInput);
+        Vector3 moveDestination = transform.position + movement;
+
+        if (agent.velocity.sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.rotation = Quaternion.LookRotation(agent.velocity.normalized);
+        }
+
+        agent.destination = moveDestination; 
     }
 }
